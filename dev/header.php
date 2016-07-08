95fe2001ae4bd0be9c541c03a2fd5a33348a1333
Vladimir <v0ffk4@outlook.com>
2016-07-08 10:58:22 +0300

    Added stylesheet link

--- dev/header.php
+++ dev/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>This is a test</title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_directory' )/style.css ?>">
 </head>
 <body>
 	<div id="wrapper">

523633e168c74dc8b1f849471a2f55bb1d54f5ac
v0ffk4 <v0ffk4@outlook.com>
2017-01-12 19:24:04 +0300

    Error fixing

--- dev/tpl/header.php
+++ dev/tpl/header.php
@@ -2,7 +2,14 @@
 <html>
 <head>
 	<title>This is a test</title>
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_directory' )/style.css ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php endif; ?>
+	<?php wp_head(); ?>
+	<link href="<?php bloginfo( 'template_directory' ); ?>/style.css" rel="stylesheet">
 </head>
 <body>
 	<div id="wrapper">
